104d994587be98da0b6fcb8cd93fd359a5e13d14
tatianenm <tatianenmlua@hotmail.com>
2024-02-13 18:44:17 -0300

    Initial commit

---
+++ src/main/java/br/com/coffeeandit/transactionsvc/TransactionSvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.coffeeandit.transactionsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionSvcApplication.class, args);
+	}
+
+}
